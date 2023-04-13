35ea389a4cda06f8016915d4bb8f35054b917450
Eynemy <mateuszzimny27@gmail.com>
2023-04-13 19:44:01 +0200

    poprawki styli

---
+++ skrypty/draw_sierpinski.js
@@ -0,0 +1,40 @@
+var canvas2 = document.getElementById("trojkat");
+var ctx2 = canvas2.getContext("2d");
+var width2 = canvas2.width;
+var height2 = canvas2.height;
+var level2 = 0;
+
+drawTriangle2(0, 0, width2, height2, level2);
+
+function drawTriangle2(x, y, w, h, level) {
+    if (level == 0) {
+        ctx2.beginPath();
+        ctx2.moveTo(x, y);
+        ctx2.lineTo(x + w, y);
+        ctx2.lineTo(x + w / 2, y + h);
+        ctx2.closePath();
+        ctx2.fillStyle = "#006400"; //zmiana koloru trojkata
+        ctx2.fill();
+    } else {
+        drawTriangle2(x, y, w / 2, h / 2, level - 1);
+        drawTriangle2(x + w / 2, y, w / 2, h / 2, level - 1);
+        drawTriangle2(x + w / 4, y + h / 2, w / 2, h / 2, level - 1);
+    }
+}
+function nextIteration() {
+    if (level2 < 7) {
+        level2++;
+        clearCanvas2();
+        drawTriangle2(0, 0, width2, height2, level2);
+    }
+}
+
+function reset() {
+    level2 = 0;
+    clearCanvas2();
+    drawTriangle2(0, 0, width2, height2, level2);
+}
+
+function clearCanvas2() {
+    ctx2.clearRect(0, 0, width2, height2);
+}
--- skrypty/trojkat.js
+++ skrypty/trojkat.js
@@ -1,20 +1,21 @@
-const canvas = document.getElementById("canvas");
+/*
+const canvas = document.getElementById("trojkat-logo-canvas");
 const ctx = canvas.getContext("2d");
 const width = canvas.width;
 const height = canvas.height;
 const maxLevel = 5;
 const delay = 10000 / (3 ** maxLevel - 1);
 
-canvas.addEventListener("click", reset);
+canvas.addEventListener("click", reset1);
 
-async function reset() {
+async function reset1() {
     // wyczyść płótno   
     ctx.clearRect(0, 0, width, height);
     // animacja
-    await drawSierpinski(0, height, width, height, width / 2, 0, maxLevel);
+    await drawSierpinski1(0, height, width, height, width / 2, 0, maxLevel);
 }
 
-async function drawSierpinski(x1, y1, x2, y2, x3, y3, level) {
+async function drawSierpinski1(x1, y1, x2, y2, x3, y3, level) {
     if (level <= 0) {
         // ustaw kolor na:
         ctx.strokeStyle = "white";
@@ -27,6 +28,7 @@ async function drawSierpinski(x1, y1, x2, y2, x3, y3, level) {
         ctx.stroke();
         // opóźnienie
         await new Promise(resolve => setTimeout(resolve, delay));
+    
     } else {
         // dziel trójkąt na trzy mniejsze trójkąty
         const x12 = (x1 + x2) / 2;
@@ -35,12 +37,54 @@ async function drawSierpinski(x1, y1, x2, y2, x3, y3, level) {
         const y23 = (y2 + y3) / 2;
         const x31 = (x3 + x1) / 2;
         const y31 = (y3 + y1) / 2;
-        // rysuj trójkąty
-        await drawSierpinski(x1, y1, x12, y12, x31, y31, level - 1);
-        await drawSierpinski(x2, y2, x23, y23, x12, y12, level - 1);
-        await drawSierpinski(x3, y3, x31, y31, x23, y23, level - 1);
+        // rysuj trójkąty 
+        
+        await drawSierpinski1(x1, y1, x12, y12, x31, y31, level - 1);
+        
+        await drawSierpinski1(x2, y2, x23, y23, x12, y12, level - 1);
+        await drawSierpinski1(x3, y3, x31, y31, x23, y23, level - 1);
     }
 }
 
 // uruchom animację
-reset();
+reset1();
+*/
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var size = 50;
+var maxDepth = 0;
+
+ctx.fillStyle = "white";
+
+function drawTriangle(x, y, size, depth) {
+  if (depth === maxDepth) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + size / 2, y + size);
+    ctx.lineTo(x - size / 2, y + size);
+    ctx.closePath();
+    ctx.fill();
+  } else {
+    var newSize = size / 2;
+    drawTriangle(x, y, newSize, depth + 1);
+    drawTriangle(x - newSize / 2, y + newSize, newSize, depth + 1);
+    drawTriangle(x + newSize / 2, y + newSize, newSize, depth + 1);
+  }
+}
+
+function drawSierpinski() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawTriangle(canvas.width / 2, size / 2, size, 0);
+  if (maxDepth < 4) {
+    maxDepth++;
+    setTimeout(drawSierpinski, 1000);
+  }
+}
+
+canvas.addEventListener("click", function() {
+  maxDepth = 0;
+  drawSierpinski();
+});
+
+drawSierpinski();
