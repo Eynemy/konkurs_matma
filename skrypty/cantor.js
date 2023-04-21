b0d44c52e6dd5715a1515a64198f6d70f2842113
Eynemy <mateuszzimny27@gmail.com>
2023-04-21 22:32:19 +0200

    version 0.510

--- skrypty/cantor.js
+++ skrypty/cantor.js
@@ -1,23 +1,25 @@
-var canvas3 = document.getElementById("Cantor-Canvas3");
-var ctx3 = canvas3.getContext("2d");
-var depth = 0;
-function drawCantorSet(x, y, len, depth) {
-    if (depth === 0) {
+// Tworzenie zbioru Cantora
+var canvas3 = document.getElementById("Cantor-Canvas3"); // Odniesienie do elementu canvas
+var ctx3 = canvas3.getContext("2d"); // Uzyskanie kontekstu rysowania 2D dla elementu canvas
+var depth = 0; // Zmienna przechowująca iteracje
+function drawCantorSet(x, y, len, depth) { // Funkcja rysująca trójkąt Sierpińskiego o zadanej pozycji i długości
+    if (depth === 0) {  // Warunek końcowy rekursji: jeśli osiągnięto zadaną iteracje, rysuj prostokąt
         ctx3.fillRect(x, y, len, 20);
-    } else {
+    } 
+    else {  // W przeciwnym razie, rysuj 3 prostokąty Sierpińskiego na pozycjach wyznaczonych na podstawie zadanej długości
         ctx3.fillRect(x, y, len, 20);
         y += 30;
         drawCantorSet(x, y, len / 3, depth - 1);
         drawCantorSet(x + len * 2 / 3, y, len / 3, depth - 1);
     }
 }
-function animate() {
-    ctx3.clearRect(0, 0, canvas3.width, canvas3.height);
-    drawCantorSet(0, 0, canvas3.width, depth);
-    depth++;
-    if (depth > 6) {
+function animate() {    // Funkcja animująca rysowanie trójkąta Sierpińskiego.
+    ctx3.clearRect(0, 0, canvas3.width, canvas3.height); // Wyczyszczenie elementu canvas przed rysowaniem
+    drawCantorSet(0, 0, canvas3.width, depth); // Narysowanie trójkąta Sierpińskiego na aktualnej iteracji
+    depth++; // Zwiększenie iteracji o 1
+    if (depth > 6) { // Jeśli osiągnięto maksymalną ilość iteracji (6), zresetuj ją do 0.
         depth = 0;
     }
-    setTimeout(animate, 1000);
+    setTimeout(animate, 1000);  // Ustawienie opóźnienia (1 sekunda) i ponowne wywołanie funkcji animate
 }
-animate();
+animate();  // Wywołanie funkcji animate na starcie programu
